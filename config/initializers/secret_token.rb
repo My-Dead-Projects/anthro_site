fcc4c8ff6efd814fd5f8551299c29193e1f2b8cd
anthropomorphic <code@mikefdorst.com>
2014-01-30 18:17:56 -0800

    Initialize Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AnthroSite::Application.config.secret_key_base = 'b5a37e1efbece6442cffa371bc9b8301912735f33ab1e097868169f0fd625b9a488ba9e767e11d158781761964151aaf3f30b8ff4acd2a1a82d234682fd96b25'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AnthroSite::Application.config.session_store :cookie_store, key: '_anthro_site_session'
